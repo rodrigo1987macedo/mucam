3692c53cc0966c6cac981933452c7cb36a40adf9
Rodrigo Macedo <rodrigo1987macedo@gmail.com>
2020-07-23 07:15:58 -0300

    limit -1 debugger

--- frontend/pages/debugger.js
+++ frontend/pages/debugger.js
@@ -6,7 +6,7 @@ import Title from "../components/common/Title";
 const cookies = new Cookies();
 
 function Debugger() {
-  const [usersInfo, setUsersInfo] = useState();
+  const [usersInfo, setUsersInfo] = useState([]);
 
   function updateUsers() {
     usersInfo.map(info => {
@@ -29,26 +29,36 @@ function Debugger() {
   }
 
   function getUsers() {
-    axios.get(`${process.env.API_URL}/users`).then(res => {
-      console.log(res);
-      let usersInfoArray = [];
-      res.data.map(user => {
-        usersInfoArray.push({
-          id: user.id,
-          username: user.username,
-          email: user.email.toLowerCase(),
-          name: user.name
+    axios
+      .get(`${process.env.API_URL}/users?_limit=-1`, {
+        headers: {
+          Authorization: `Bearer ${cookies.get("guards")}`
+        }
+      })
+      .then(res => {
+        let usersInfoArray = [];
+        res.data.map(user => {
+          usersInfoArray.push({
+            id: user.id,
+            username: user.username,
+            email: user.email.toLowerCase(),
+            name: user.name
+          });
         });
+        setUsersInfo(usersInfoArray);
+        console.log("finished");
       });
-      console.log("usersInfoArray: ", usersInfoArray);
-      setUsersInfo(usersInfoArray);
-    });
+  }
+
+  function seeUsersInfo() {
+    console.log(usersInfo);
   }
 
   return (
     <>
       <Title text="Debugger" tag="h1" />
       <button onClick={() => getUsers()}>getUsers</button>
+      <button onClick={() => seeUsersInfo()}>seeUsersInfo</button>
       <button onClick={() => updateUsers()}>updateUsers</button>
     </>
   );
